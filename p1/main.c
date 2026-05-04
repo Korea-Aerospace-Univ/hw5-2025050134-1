25bc9a3984fd84e88e24a0cb468c6308d74da60b
2025050134 <yoonga06@kau.kr>
2026-05-04 15:19:55 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int N;
+    int found = 0;
+    
+    scanf("%d", &N);
+
+    for (int a = 1; a * 900 < N; a++) {
+        for (int b = 2; a * 900 + b * 750 < N; b += 2) {
+            for (int c = 1; a * 900 + b * 750 + c * 200 <= N; c++) {
+                if (a * 900 + b * 750 + c * 200 == N) {
+                    if (c < a || c < b) {
+                        printf("%d %d %d\n", a, b, c);
+                        found = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    if (found == 0) {
+        printf("none\n");
+    }
+
+    return 0;
+}
